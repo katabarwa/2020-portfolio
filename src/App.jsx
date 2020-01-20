0987c2c85264cba30749767fefb535c4def8d2ed
Jesse Katabarwa <mail@jessekatabarwa.info>
2020-01-20 13:32:15 -0500

    first project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
-    render = () => {
-        return "Hello world!"
-    }
+  render = () => {
+    return (
+      <div>
+        <div className="header">
+          <p>JESSE KATABARWA</p>
+          <button>ABOUT</button>
+        </div>
+        <div className="project-1">
+          <div className="flex-title">
+            <p>DIASPHORA</p>
+            <p className="italics">Triptych on Diasporic Identities</p>
+          </div>
+          <img width="100%" src="/diasphora.png" />
+          <p className="text">
+            Diasphora is a research project in exploring the ways that black and
+            east asian identities are interpolated by mainstream narratives. The
+            project represents the essential gap between these narratives and
+            the realms of personal experience, emphasizing the hegemonic gaze
+            upon othered bodies. The gaze becomes a spectacle within itself at
+            the same time that the gaze is physically imposed upon the viewer.
+            <br /> diasphora (adj.) diaspora + dyphoria The unease or feeling of
+            having no sense of home; forever displaced, forever the other,
+            without any sense of self; a blurred identity, a dismantling of the
+            body.
+          </p>
+        </div>
+        <div className="project-2"></div>
+      </div>
+    );
+  };
 }
 
-
-export default App
+export default App;
